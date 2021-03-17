32ce6ad3bdd6a0b84f7c9b15a5e3cea205379fce
Musthavename <musthavename@hotmail.co.uk>
2021-03-17 10:56:41 +0000

    Disabled slacken recruitment.
    Yes, the replacement decision doesn't exist yet.

---
+++ common/defines/00_mmr.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+-- Put this at >1 which makes it impossible to click.
+NDefines.NMilitary.SLACKEN_AP_DROP = 1.1
