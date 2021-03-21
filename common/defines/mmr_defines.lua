0df948be58813acc6eb6de77ee101f84635e17fd
Musthavename <musthavename@hotmail.co.uk>
2021-03-21 14:26:32 +0000

    Few more balance tweaks - Changes to Prussian militarisation and revolution target (tidied up the order of the file to make it consistent with vanilla) - Alliances can now be done regardless of relations (i.e. players can ally at will, provided they are not rivals) - Changing rivals can be done more frequently (10 years instead of 25 years) and cheaply (50 dip instead of 100 dip).

--- common/defines/mmr_defines.lua
+++ common/defines/mmr_defines.lua
@@ -4,3 +4,12 @@
 
 -- Put this at >1 which makes it impossible to click.
 NDefines.NMilitary.SLACKEN_AP_DROP = 1.1
+
+-- Changed from 25 years to 10 years to make a bit more flexible to players.
+NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 10
+
+-- Put this at -200 so nations (particularly players) can always ally.
+NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
+
+-- Decreased the change rival cost down from 100 to 50.
+NDefines.NCountry.PS_REPLACE_RIVAL = 50
